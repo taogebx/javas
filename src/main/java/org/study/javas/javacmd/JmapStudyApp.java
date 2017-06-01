cfbddd9cc65c8c8442f2f6c0bf232a5dbf31aec0
taogebx <taogebx@126.com>
2017-06-01 21:41:39 +0800

    add ignore and start jmap study

---
+++ src/main/java/org/study/javas/javacmd/JmapStudyApp.java
@@ -0,0 +1,10 @@
+package org.study.javas.javacmd;
+
+/**
+ * Created by taogebx on 6/1/17.
+ */
+public class JmapStudyApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/study/javas/process/ProcessStudyApp.java
@@ -0,0 +1,10 @@
+package org.study.javas.process;
+
+/**
+ * Created by taogebx on 6/1/17.
+ */
+public class ProcessStudyApp {
+    public static void main(String[] args) {
+
+    }
+}
